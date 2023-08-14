3f91f22e5e2aa51dbf7ff6eb62358631fa01bb23
adricarrera <adriicarrerafdez@gmail.com>
2023-08-14 16:28:30 +0200

    Añadir preguntas incompleto

--- front/src/app/main/questions/questions-routing.module.ts
+++ front/src/app/main/questions/questions-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { QuestionsHomeComponent } from './questions-home/questions-home.component';
+import { QuestionsDetailComponent } from './questions-detail/questions-detail.component';
+import { QuestionsNewComponent } from './questions-new/questions-new.component';
 
 
 const routes: Routes = [{
   path: '',
   component: QuestionsHomeComponent
+},
+{
+  path: "new",
+  component: QuestionsNewComponent
+},
+{
+  path: ":question_code",
+  component: QuestionsDetailComponent
 }];
 
 @NgModule({
--- front/src/app/main/questions/questions.module.ts
+++ front/src/app/main/questions/questions.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { QuestionsRoutingModule } from './questions-routing.module';
 import { QuestionsHomeComponent } from './questions-home/questions-home.component';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { QuestionsDetailComponent } from './questions-detail/questions-detail.component';
+import { QuestionsNewComponent } from './questions-new/questions-new.component';
 
 
 @NgModule({
-  declarations: [QuestionsHomeComponent],
+  declarations: [QuestionsHomeComponent, QuestionsDetailComponent, QuestionsNewComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
