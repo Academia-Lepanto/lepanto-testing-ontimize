fc51d2e83802fd8d31b822d39ab96a8d1eaa74eb
Daniel Martínez <dani.martinez.3.fernandez@gmail.com>
2023-08-30 00:18:50 +0200

    Delete front to deploy only back

--- front/src/app/shared/app.menu.config.ts
+++
@@ -1,8 +0,0 @@
-import { MenuRootItem } from 'ontimize-web-ngx';
-
-export const MENU_CONFIG: MenuRootItem[] = [
-  { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'users', name: 'USERS', icon: 'people', route: '/main/users' },
-  { id: 'questions', name: 'QUESTIONS', icon: 'info_outline', route: '/main/questions' },
-  { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' },
-];
