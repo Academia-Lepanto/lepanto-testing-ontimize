147128775620963ddcf95e62e5e1479099234b8e
adricarrera <adriicarrerafdez@gmail.com>
2023-09-20 04:13:49 +0200

    new category front

--- front/src/app/main/categories/categories.module.ts
+++ front/src/app/main/categories/categories.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesHomeComponent } from './categories-home/categories-home.component';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { CategoriesNewComponent } from './categories-new/categories-new.component';
 
 
 @NgModule({
-  declarations: [CategoriesHomeComponent],
+  declarations: [CategoriesHomeComponent, CategoriesNewComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
