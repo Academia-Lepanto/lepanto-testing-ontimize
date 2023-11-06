0eab8f859363401103e825a8175e3829a8230995
adricarrera <adriicarrerafdez@gmail.com>
2023-11-06 14:05:40 +0100

    Arreglo question_code

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -5,7 +5,8 @@ import { SERVICE_CONFIG } from './shared/app.services.config';
 
 export const CONFIG: Config = {
   // The base path of the URL used by app services.
-  apiEndpoint: 'http://localhost:8080',
+  apiEndpoint: 'https://lepanto-back-api-c8d157e0a5c4.herokuapp.com',
+  //apiEndpoint: 'http://localhost:8080',
   // Application identifier. Is the unique package identifier of the app.
   // It is used when storing or managing temporal data related with the app.
   // By default is set as 'ontimize-web-uuid'.
